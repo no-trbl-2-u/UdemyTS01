f6564c48343c21b0828442d8bdcdeb24bd7f1a0a
unknown <no.trbl.2.u@gmail.com>
2019-04-02 14:51:00 -0400

    Finished Module 05

--- Modules/05module.js
+++ Modules/05module.js
@@ -15,14 +15,16 @@ var __extends = (this && this.__extends) || (function () {
 var Mammel = /** @class */ (function () {
     function Mammel(name, username) {
         this.username = username;
-        // __protected__ means it can not be targetted outside the class
+        // __protected__ means it can NOT be targetted outside the class
         // *** BUT *** it CAN be targetted in an inherited class
         this.age = 27;
         // Property defined here based on constructor
         this.name = name;
-        // Unnecassary because the 
-        // __declaration__ && __definition__ is in the constructor parameters
-        // ### this.username = username ### 
+        /*
+          this.username = username
+            Unnecassary because the __declaration__ &&
+            __definition__ is in the constructor parameters
+        */
     }
     Mammel.prototype.printAge = function () {
         console.log(this.age);
@@ -35,8 +37,8 @@ var Mammel = /** @class */ (function () {
     return Mammel;
 }());
 var doggy = new Mammel("Dog", "doggyUser");
-console.log(doggy);
-doggy.printAge();
+// console.log(doggy)
+// doggy.printAge();
 //__________________________________________________________________
 // Inheritance
 var Dolphin = /** @class */ (function (_super) {
@@ -46,11 +48,133 @@ var Dolphin = /** @class */ (function (_super) {
     // name = "dolphyInside"
     function Dolphin(username) {
         var _this = _super.call(this, "dolphyInside", username) || this;
+        // __private__
+        // age is a property from the parent class that CAN be targetted
         _this.age = 31;
         return _this;
-        // this.type = "dolphin" -- Property 'type' is private and only accessible within class 'Mammel'
+        // __protected__
+        // Property 'type' is private and only accessible within class 'Mammel'
+        // this.type = "dolphin" // !ERROR! 
     }
     return Dolphin;
 }(Mammel));
 var max = new Dolphin("dolphyUser");
-console.log(max);
+// console.log(max)
+//__________________________________________________________________________
+// Getters and Setters
+/*
+  Instead of calling the __getter__ and __setter__ methods directly,
+  you get and set like you would normally, BUT by
+  writing these getters and setters, you can create rules
+  and procedures for class manipulation
+*/
+var Plant = /** @class */ (function () {
+    function Plant() {
+        // props
+        this._species = "Default Value";
+    }
+    Object.defineProperty(Plant.prototype, "species", {
+        // __getter__
+        get: function () {
+            return this._species;
+        },
+        // __setter__
+        set: function (value) {
+            if (value.length > 3) {
+                this._species = value;
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Plant;
+}());
+var dandy = new Plant();
+dandy.species = "RE"; // Doesn't pass __setter_ rules
+// console.log(dandy.species) // Default Value
+dandy.species = "Dandelion";
+// console.log(dandy.species) // Dandelion
+// ________________________________________________________
+// Static Properties and Methods
+/*
+  The __static__ keyword allows you to use properties and methods
+  WITHOUT instantiating the class that holds them
+*/
+var Helpers = /** @class */ (function () {
+    function Helpers() {
+    }
+    Helpers.calcCircumference = function (diameter) {
+        return this.PI * diameter;
+    };
+    Helpers.PI = 3.145;
+    return Helpers;
+}());
+// console.log(Helpers.calcCircumference(100)) // 314.5
+// console.log(Helpers.PI) // 3.145
+//______________________________________________________________
+// Abstract Classes
+/*
+  Abstract Classes CAN NOT be instantiated directly.
+  They can ONLY be inherited from. Maybe the class
+  NEVER NEEDS to be instantiated, but can hold
+  properties and methods that all your child
+  classes will need. Great for a __BLUEPRINT__ of a class
+  you'd like to create/inherit from.
+*/
+var Project = /** @class */ (function () {
+    function Project() {
+        this.projectName = "Default";
+    }
+    // normal methods can be called and used by child classes
+    Project.prototype.calcBudget = function () {
+        return this.budget * 2;
+    };
+    return Project;
+}());
+// EXAMPLE
+var ITProject = /** @class */ (function (_super) {
+    __extends(ITProject, _super);
+    function ITProject() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    ITProject.prototype.changeName = function (name) {
+        this.projectName = name;
+    };
+    return ITProject;
+}(Project));
+var itProject = new ITProject();
+// console.log(itProject.projectName) // Default
+itProject.changeName("IT Project");
+// console.log(itProject.projectName) // IT Project 
+//_______________________________________________________
+// Private Constructors, Singleton Classes, readonly
+/*
+  Singleton Classes will allow you to have only one instance
+  during runtime. The __private construtor__ allows you to
+  enforce that and stops you from using the __new__ keyword.
+  You can only use the __new__ keyword from INSIDE the class
+  definition. __readonly__ property allows you to have
+  immutable properties within your class. ***Note*** __readonly__
+  CAN NOT be declared inside of the constructor parameters.
+  That shorthand is not allowed.
+*/
+var OnlyOne = /** @class */ (function () {
+    // CAN NOT be instantiatied OUTSIDE of the class definition
+    function OnlyOne(name) {
+        this.name = name;
+    }
+    // !instance ? make one : return it.
+    OnlyOne.getInstance = function () {
+        if (!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne('The Only One');
+        }
+        return OnlyOne.instance;
+    };
+    return OnlyOne;
+}());
+// let wrong = new OnlyOne('The Only One') // !ERROR!
+var right = OnlyOne.getInstance();
+console.log(right.name); // 'The Only One'
+// Cannot assign to 'name' because it is a read-only property.
+// right.name = "New Name"
+//__________________________________________________________________
--- Modules/05module.ts
+++ Modules/05module.ts
@@ -1,21 +1,25 @@
 class Mammel {
-  // Property declared here...Must be included in constructor
+  // Property declared here...
+  // Must be included in constructor
+  // OR a default value must be provided
   name: string;
 
-  // __private__ means it can not be targetted outside the class
-  private type!: string;
+  // __private__ means it can NOT be targetted outside the class
+  private type!: string; // the "!" is to quiet the compiler
   
-  // __protected__ means it can not be targetted outside the class
+  // __protected__ means it can NOT be targetted outside the class
   // *** BUT *** it CAN be targetted in an inherited class
   protected age: number = 27;
 
   constructor(name: string, public username: string){
     // Property defined here based on constructor
     this.name = name;
-
-    // Unnecassary because the 
-    // __declaration__ && __definition__ is in the constructor parameters
-    // ### this.username = username ### 
+    
+  /*
+    this.username = username
+      Unnecassary because the __declaration__ &&
+      __definition__ is in the constructor parameters
+  */ 
   }
 
   printAge() {
@@ -30,8 +34,8 @@ class Mammel {
 }
 
 const doggy = new Mammel("Dog", "doggyUser")
-console.log(doggy)
-doggy.printAge();
+// console.log(doggy)
+// doggy.printAge();
 
 //__________________________________________________________________
 
@@ -43,10 +47,150 @@ class Dolphin extends Mammel{
 
   constructor(username: string){
     super("dolphyInside", username);
-    this.age = 31
-    // this.type = "dolphin" -- Property 'type' is private and only accessible within class 'Mammel'
+    
+    // __private__
+    // age is a property from the parent class that CAN be targetted
+    this.age = 31 
+    
+    // __protected__
+    // Property 'type' is private and only accessible within class 'Mammel'
+    // this.type = "dolphin" // !ERROR! 
   }
 }
 
 const max = new Dolphin("dolphyUser");
-console.log(max)
+// console.log(max)
+//__________________________________________________________________________
+
+// Getters and Setters
+
+/*
+  Instead of calling the __getter__ and __setter__ methods directly,
+  you get and set like you would normally, BUT by
+  writing these getters and setters, you can create rules
+  and procedures for class manipulation 
+*/
+
+class Plant {
+  // props
+  private _species: string = "Default Value";
+
+  // __getter__
+  get species() {
+    return this._species
+  }
+
+  // __setter__
+  set species(value: string) {
+    if(value.length > 3){
+      this._species = value  
+    }
+  }
+}
+
+const dandy = new Plant()
+dandy.species = "RE" // Doesn't pass __setter_ rules
+// console.log(dandy.species) // Default Value
+
+dandy.species = "Dandelion"
+// console.log(dandy.species) // Dandelion
+// ________________________________________________________
+
+// Static Properties and Methods
+
+/*
+  The __static__ keyword allows you to use properties and methods
+  WITHOUT instantiating the class that holds them
+*/
+
+class Helpers {
+  static PI: number = 3.145;
+  
+  static calcCircumference(diameter: number): number {
+    return this.PI * diameter
+  }
+}
+
+// console.log(Helpers.calcCircumference(100)) // 314.5
+// console.log(Helpers.PI) // 3.145
+//______________________________________________________________
+
+// Abstract Classes
+/* 
+  Abstract Classes CAN NOT be instantiated directly.
+  They can ONLY be inherited from. Maybe the class
+  NEVER NEEDS to be instantiated, but can hold 
+  properties and methods that all your child 
+  classes will need. Great for a __BLUEPRINT__ of a class
+  you'd like to create/inherit from.
+*/
+
+abstract class Project {
+  projectName: string = "Default";
+  budget!: number;
+
+  // abstract method doesn't define the method.
+  // It only defines the type for the child method
+  abstract changeName(name: string): void;
+
+  // normal methods can be called and used by child classes
+  calcBudget() {
+    return this.budget * 2
+  }
+}
+
+// EXAMPLE
+class ITProject extends Project {
+  changeName(name: string) {
+    this.projectName = name
+  }
+}
+
+const itProject = new ITProject()
+// console.log(itProject.projectName) // Default
+itProject.changeName("IT Project")
+// console.log(itProject.projectName) // IT Project 
+//_______________________________________________________
+
+// Private Constructors, Singleton Classes, readonly
+
+/*
+  Singleton Classes will allow you to have only one instance
+  during runtime. The __private construtor__ allows you to
+  enforce that and stops you from using the __new__ keyword.
+  You can only use the __new__ keyword from INSIDE the class
+  definition. __readonly__ property allows you to have
+  immutable properties within your class. ***Note*** __readonly__
+  CAN NOT be declared inside of the constructor parameters.
+  That shorthand is not allowed.
+*/
+
+class OnlyOne {
+  // The instantiation of the class is of Type...this class!
+  private static instance: OnlyOne;
+
+  // readonly
+  public readonly name: string;
+
+  // CAN NOT be instantiatied OUTSIDE of the class definition
+  private constructor(name: string) {
+    this.name = name;
+  }
+
+  // !instance ? make one : return it.
+  static getInstance() {
+    if(!OnlyOne.instance) {
+      OnlyOne.instance = new OnlyOne('The Only One');
+    }
+    return OnlyOne.instance
+  }
+}
+
+// let wrong = new OnlyOne('The Only One') // !ERROR!
+let right = OnlyOne.getInstance()
+console.log(right.name) // 'The Only One'
+
+// Cannot assign to 'name' because it is a read-only property.
+// right.name = "New Name"
+
+//__________________________________________________________________
